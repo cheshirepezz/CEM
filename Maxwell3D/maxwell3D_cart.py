65159dc21c301610d1c13e64a7d649fc1b67c1d5
Luca Pezzini <cheshirepezz@protonmail.com>
2020-09-09 18:20:03 +0200

    Maxwell 3D Yee

--- Maxwell3D/maxwell3D_cart.py
+++ Maxwell3D/maxwell3D_cart.py
@@ -1,6 +1,6 @@
 #
 # created:        08.09.2020
-# last modified:  08.09.2020
+# last modified:  09.09.2020
 # author:         Luca Pezzini
 # e-mail :        luca.pezzini@edu.unito.it
 # MIT license
@@ -34,48 +34,30 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import pyplot, cm
-from matplotlib import animation as anim
 from mpl_toolkits.mplot3d import Axes3D
 
 # Function def.
 
-def energy(p):
+def Energy(p):
     plt.figure()
     plt.plot(p)
-    plt.title("Total Energy in Time")
+    plt.title("Energy Conservation")
     plt.xlabel("time")
-    plt.ylabel("Utot")
+    plt.ylabel("U")
 
-def plot2D(x, y, p):
+def DivB(p):
     plt.figure()
-    plt.contourf(x, y, p)#, cmap=plt.cm.jet)
-    plt.title("Contour of Bz")
-    plt.xlabel("y")
-    plt.ylabel("x")
-    plt.colorbar()
-
-def plot3D(x, y, p):
-    fig = pyplot.figure(figsize=(11, 7), dpi=100)
-    ax = fig.gca(projection='3d')
-    surf = ax.plot_surface(x, y, p[:], rstride=1, cstride=1, cmap=cm.viridis,
-            linewidth=0, antialiased=False)
-    ax.view_init(30, 225)
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$y$')
-    plt.show()
-    
-# Animation setup
-#fig = plt.figure('frame')
-#plt.rcParams['animation.ffmpeg_path'] = "/Users/luca_pezzini/Documents/00-plasma_phy/covariant_pic-2d"
-#plt.rcParams['animation.bitrate'] = 100000
-#ims = []
+    plt.plot(p)
+    plt.title("Mimetic Operator")
+    plt.xlabel("time")
+    plt.ylabel("div(B)")
 
 #
 # STEP 1: SET the GRID!
 #
 
-Nt = 1000 # number of time steps
-Nx, Ny, Nz = 501, 501, 501  # nodes
+Nt = 800 # number of time steps
+Nx, Ny, Nz = 51, 51, 51  # nodes
 sigma = 0.02
 xmin, xmax = 0, 1 # physic domain x
 ymin, ymax = 0, 1 # physic domain y
@@ -104,7 +86,7 @@ print("Increments (dx, dy, dz):", dx, dy, dz)
 x = np.linspace(xmin, xmax, Nx, dtype=float)
 y = np.linspace(ymin, ymax, Ny, dtype=float)
 z = np.linspace(zmin, zmax, Nz, dtype=float)
-xv, yv, zv = np.meshgrid(x, y, z, indexing='ijk')
+xv, yv, zv = np.meshgrid(x, y, z, indexing='ij')
 
 # Initialization of field matrices
 Ex = np.zeros([Nx, Ny, Nz], dtype=float)
@@ -113,24 +95,33 @@ Ez = np.zeros([Nx, Ny, Nz], dtype=float)
 Bx = np.zeros([Nx, Ny, Nz], dtype=float)
 By = np.zeros([Nx, Ny, Nz], dtype=float)
 Bz = np.zeros([Nx, Ny, Nz], dtype=float)
+Bxold = np.zeros([Nx, Ny, Nz], dtype=float)
+Byold = np.zeros([Nx, Ny, Nz], dtype=float)
 Bzold = np.zeros([Nx, Ny, Nz], dtype=float)
 
 # Swop variable
-Ex_l = np.zeros([Nx, Ny, Nz], dtype=float)
+Ex_w = np.zeros([Nx, Ny, Nz], dtype=float)
+Ex_s = np.zeros([Nx, Ny, Nz], dtype=float)
 Ex_b = np.zeros([Nx, Ny, Nz], dtype=float)
-Ey_l = np.zeros([Nx, Ny, Nz], dtype=float)
+Ey_w = np.zeros([Nx, Ny, Nz], dtype=float)
+Ey_s = np.zeros([Nx, Ny, Nz], dtype=float)
 Ey_b = np.zeros([Nx, Ny, Nz], dtype=float)
-Ez_l = np.zeros([Nx, Ny, Nz], dtype=float)
+Ez_w = np.zeros([Nx, Ny, Nz], dtype=float)
+Ez_s = np.zeros([Nx, Ny, Nz], dtype=float)
 Ez_b = np.zeros([Nx, Ny, Nz], dtype=float)
-Bx_l = np.zeros([Nx, Ny, Nz], dtype=float)
+Bx_w = np.zeros([Nx, Ny, Nz], dtype=float)
+Bx_s = np.zeros([Nx, Ny, Nz], dtype=float)
 Bx_b = np.zeros([Nx, Ny, Nz], dtype=float)
-By_l = np.zeros([Nx, Ny, Nz], dtype=float)
+By_w = np.zeros([Nx, Ny, Nz], dtype=float)
+By_s = np.zeros([Nx, Ny, Nz], dtype=float)
 By_b = np.zeros([Nx, Ny, Nz], dtype=float)
-Bz_l = np.zeros([Nx, Ny, Nz], dtype=float)
+Bz_w = np.zeros([Nx, Ny, Nz], dtype=float)
+Bz_s = np.zeros([Nx, Ny, Nz], dtype=float)
 Bz_b = np.zeros([Nx, Ny, Nz], dtype=float)
 
 Bz[int((Nx - 1)/2),int((Ny - 1)/2), int((Nz - 1)/2)] = 0.001 # Initial conditions
-Utot = np.zeros([Nt], dtype=float) # Total Energy
+U = np.zeros([Nt], dtype=float) # Total energy
+divB = np.zeros([Nt], dtype=float) # Divergence of B
 
 #
 # STEP 3: TIME EVOLUTION OF THE FIELD ON THE GRID!
@@ -145,6 +136,8 @@ zs, ze = 1, Nz - 1
 print("START SYSTEM EVOLUTION!")
 for t in range(Nt): # count {0, Nt-1}
 
+    Bxold[:, :, :] = Bx[:, :, :]
+    Byold[:, :, :] = By[:, :, :]
     Bzold[:, :, :] = Bz[:, :, :]
 
     # BEGIN : spatial update loops for Ey and Ex fields
@@ -154,73 +147,84 @@ for t in range(Nt): # count {0, Nt-1}
                              - (dt/dx) * (Bz[xs+1:xe+1, ys:ye, zs:ze] - Bz[xs:xe, ys:ye, zs:ze])
     Ez[xs:xe, ys:ye, zs:ze] += (dt/dx) * (By[xs+1:xe+1, ys:ye, zs:ze] - By[xs:xe, ys:ye, zs:ze])\
                              - (dt/dy) * (Bx[xs:xe, ys+1:ye+1, zs:ze] - Bx[xs:xe, ys:ye, zs:ze])
- 
+
     # swop var.
-    Ex_l[0, :, :] = Ex[0, :, :]
-    Ex_b[:, 0, :] = Ex[:, 0, :]
-    Ey_l[0, :, :] = Ey[0, :, :]
-    Ey_b[:, 0, :] = Ey[:, 0, :]
-    Ez_l[:, :, 0] = Ez[:, :, 0]
-    Ez_b[:, 0] = Ez[:, 0]
+    Ex_w[0, :, :] = Ex[0, :, :]
+    Ex_s[:, 0, :] = Ex[:, 0, :]
+    Ex_b[:, :, 0] = Ex[:, :, 0]
+    Ey_w[0, :, :] = Ey[0, :, :]
+    Ey_s[:, 0, :] = Ey[:, 0, :]
+    Ey_b[:, :, 0] = Ey[:, :, 0]
+    Ez_w[0, :, :] = Ez[0, :, :]
+    Ez_s[:, 0, :] = Ez[:, 0, :]
+    Ez_b[:, :, 0] = Ez[:, :, 0]
     # Reflective BC for E field
-    Ex[0, :] = Ex[-1, :]   # left = right
-    Ex[-1, :] = Ex_l[0, :] # right = left
-    Ex[:, 0] = Ex[:, -1]   # bottom=top
-    Ex[:, -1] = Ex_b[:, 0] # top = bottom
-    Ey[0, :] = Ey[-1, :]   # left = right
-    Ey[-1, :] = Ey_l[0, :] # right = left
-    Ey[:, 0] = Ey[:, -1]   # bottom = top
-    Ez[:, -1] = Ez_b[:,0]  # top = bottom
-    Ez[0, :] = Ez[-1, :]   # left = right
-    Ez[-1, :] = Ez_l[0, :] # right = left
-    Ez[:, 0] = Ez[:, -1]   # bottom = top
-    Ez[:, -1] = Ez_b[:,0]  # top = bottom
-    
+    Ex[0, :, :] = Ex[-1, :, :]   # west = est
+    Ex[-1, :, :] = Ex_w[0, :, :] # est = west
+    Ex[:, 0, :] = Ex[:, -1, :]   # south = north
+    Ex[:, -1, :] = Ex_s[:, 0, :] # north = south
+    Ex[:, :, 0] = Ex[:, :, -1]   # bottom = top
+    Ex[:, :, -1] = Ex_b[:, :, 0] # top = bottom   
+    Ey[0, :, :] = Ey[-1, :, :]   # west = est
+    Ey[-1, :, :] = Ey_w[0, :, :] # est = west
+    Ey[:, 0, :] = Ey[:, -1, :]   # south = north
+    Ey[:, -1, :] = Ey_s[:, 0, :] # north = south
+    Ey[:, :, 0] = Ey[:, :, -1]   # bottom = top
+    Ey[:, :, -1] = Ey_b[:, :, 0] # top = bottom
+    Ez[0, :, :] = Ey[-1, :, :]   # west = est
+    Ez[-1, :, :] = Ey_w[0, :, :] # est = west
+    Ez[:, 0, :] = Ey[:, -1, :]   # south = north
+    Ez[:, -1, :] = Ey_s[:, 0, :] # north = south
+    Ez[:, :, 0] = Ey[:, :, -1]   # bottom = top
+    Ez[:, :, -1] = Ey_b[:, :, 0] # top = bottom
+ 
     # BEGIN : spatial update loops for Bz fields
     # NOTE: B field is calculate with the updated E field so is half a step ahead
-    Bz[xs:xe, ys:ye] += dt * ((1/dy) * (Ex[xs:xe, ys:ye] - Ex[xs:xe, ys-1:ye-1])\
-                            - (1/dx) * (Ey[xs:xe, ys:ye] - Ey[xs-1:xe-1, ys:ye]))
-    #Bz[xs:xe, ys:ye] = Bzold[xs:xe, ys:ye] + dt * ((1/dy) * (Exold[xs:xe, ys:ye] - Exold[xs:xe, ys-1:ye-1])\
-    #                        - (1/dx) * (Eyold[xs:xe, ys:ye] - Eyold[xs-1:xe-1, ys:ye]))
-    
+    Bx[xs:xe, ys:ye, zs:ze] += dt * ((1/dz) * (Ey[xs:xe, ys:ye, zs:ze] - Ey[xs:xe, ys:ye, zs-1:ze-1])\
+                                   - (1/dy) * (Ez[xs:xe, ys:ye, zs:ze] - Ez[xs:xe, ys-1:ye-1, zs:ze]))
+    By[xs:xe, ys:ye, zs:ze] += dt * ((1/dx) * (Ez[xs:xe, ys:ye, zs:ze] - Ez[xs-1:xe-1, ys:ye, zs:ze])\
+                                   - (1/dz) * (Ex[xs:xe, ys:ye, zs:ze] - Ex[xs:xe, ys:ye, zs-1:ze-1]))
+    Bz[xs:xe, ys:ye, zs:ze] += dt * ((1/dy) * (Ex[xs:xe, ys:ye, zs:ze] - Ex[xs:xe, ys-1:ye-1, zs:ze])\
+                                   - (1/dx) * (Ey[xs:xe, ys:ye, zs:ze] - Ey[xs-1:xe-1, ys:ye, zs:ze])) 
     # END : spatial update loops for Bz fields
-    
+ 
+    # swop var.
+    Bz_w[0, :, :] = Bz[0, :, :] # west
+    Bz_s[:, 0, :] = Bz[:, 0, :] # south
+    Bz_b[:, :, 0] = Bz[:, :, 0] # bottom
     # Reflective BC for B field
-    Bz_l[0, :] = Bz[0, :]
-    Bz_b[:, 0] = Bz[:, 0]
-
-    Bz[0, :]   = Bz[-1, :] #left=right
-    Bz[-1, :]  = Bz_l[0, :] #right=left
-    Bz[:, 0]   = Bz[:, -1] #bottom=top
-    Bz[:, -1]  = Bz_b[:, 0] #top=bottom
+    Bz[0, :, :]   = Bz[-1, :, :] # west = est
+    Bz[-1, :, :]  = Bz_w[0, :, :] # est = west
+    Bz[:, 0, :]   = Bz[:, -1, :] # south = north
+    Bz[:, -1, :]  = Bz_s[:, 0, :] # north = south
+    Bz[:, :, 0]   = Bz[:, :, -1] # bottom = top
+    Bz[:, :, -1]  = Bz_b[:, :, 0] # top = bottom
 
     # Energy must conserve
-    # Time is staggered to updating E and B. So to calculate Utot 
+    # Time is staggered to updating E and B. So to calculate U 
     # we should center the value of the B field to the same point in 
     # time (i.e. B_av(t) = (B(t) + B(t+1))/2)
 
-    Utot[t] =  0.5 * np.sum( np.power(Ex[xs:xe, ys:ye],2.)\
-                      + np.power(Ey[xs:xe, ys:ye],2.)\
-                      + Bz[xs:xe, ys:ye] * Bzold[xs:xe, ys:ye])
-                     #+np.power((Bz[xs:xe, ys:ye] + Bzold[xs:xe, ys:ye])/2.,2.))
+    U[t] =  0.5 * np.sum(np.power(Ex[xs:xe, ys:ye, zs:ze],2.)\
+                          + np.power(Ey[xs:xe, ys:ye, zs:ze],2.)\
+                          + np.power(Ez[xs:xe, ys:ye, zs:ze],2.)\
+                          + Bx[xs:xe, ys:ye, zs:ze] * Bxold[xs:xe, ys:ye, zs:ze]\
+                          + By[xs:xe, ys:ye, zs:ze] * Byold[xs:xe, ys:ye, zs:ze]\
+                          + Bz[xs:xe, ys:ye, zs:ze] * Bzold[xs:xe, ys:ye, zs:ze])
+    divB[t] = np.sum((1/dx) * (Bx[xs+1:xe+1, ys:ye, zs:ze] - Bx[xs:xe, ys:ye, zs:ze])\
+                   + (1/dy) * (By[xs:xe, ys+1:ye+1, zs:ze] - By[xs:xe, ys:ye, zs:ze])\
+                   + (1/dz) * (Bz[xs:xe, ys:ye, zs+1:ze+1] - Bz[xs:xe, ys:ye, zs:ze]))
 
 print("DONE!")
 
-print("Min Energy", np.min(Utot))
-print("Max Energy", np.max(Utot))
+print("Min Energy", np.min(U))
+print("Max Energy", np.max(U))
 
 #
 # STEP 4: VISUALIZATION!
 #
 
-# Create figures
-#plot3D(xv, yv, Bz)
-plot2D(xv, yv, Bz)
-energy(Utot)
-
-# Create animation
-#an = anim.ArtistAnimation(fig, ims, interval=10, repeat_delay=0, blit=True)
-#writer = anim.FFMpegWriter(fps=30)
-#an.save('bz_vid.mp4', writer=writer, dpi=500)
+Energy(U)
+DivB(divB)
 
 plt.show()
